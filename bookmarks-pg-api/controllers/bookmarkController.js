ab73e4a3a6df87741ad517c15e32efa894b2f345
Hady Mohamed <hadymohamed@pursuit.org>
2021-07-06 22:26:21 -0400

    Finished SHOW and CREATE code along

--- bookmarks-pg-api/controllers/bookmarkController.js
+++ bookmarks-pg-api/controllers/bookmarkController.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const bookmarks = express.Router();
-const { getAllBookmarks } = require("../queries/bookmarks");
+const {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+} = require("../queries/bookmarks");
 
 // INDEX
 bookmarks.get("/", async (req, res) => {
   const allBookmarks = await getAllBookmarks();
   res.json(allBookmarks);
+});
+
+// SHOW
+bookmarks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id !== typeof "number") {
+    res
+      .status(404)
+      .json({
+        error: "Invalid id",
+        message: "Please select a number for id not a text",
+      });
+    return;
+  }
+  try {
+    const bookmark = await getBookmark(id);
+    if (bookmark["id"]) {
+      res.json(bookmark);
+    } else {
+      console.log(`Database error: ${bookmark}`);
+      throw `There is no bookmark with id: ${id}`;
+    }
+  } catch (e) {
+    res.status(404).json({ error: "Resource not found.", message: e });
+  }
+});
+
+// CREATE
+bookmarks.post("/", async (req, res) => {
+  try {
+    const bookmark = await createBookmark(req.body);
+    if (bookmark["id"]) {
+      res.json(bookmark);
+    } else {
+      console.log(`Database error: ${bookmark}`);
+      throw `Error adding ${req.body} to the database.`;
+    }
+  } catch (err) {
+    res.status(404).json({ error: err });
+  }
 });
 
 module.exports = bookmarks;
--- bookmarks-pg-api/queries/bookmarks.js
+++ bookmarks-pg-api/queries/bookmarks.js
@@ -10,6 +10,47 @@ const getAllBookmarks = async () => {
   }
 };
 
+// SHOW
+const getBookmark = async (id) => {
+  try {
+    const oneBookmark = await db.one("SELECT * FROM bookmarks WHERE id=$1", id);
+    return oneBookmark;
+  } catch (err) {
+    return err;
+  }
+};
+
+// CREATE
+const createBookmark = async (bookmark) => {
+  try {
+    if (
+      !bookmark.name ||
+      !bookmark.url ||
+      !bookmark.category ||
+      !bookmark.is_favorite
+    ) {
+      throw "You must specify a value";
+    }
+    if (
+      bookmark.name !== typeof "string" ||
+      bookmark.url !== typeof "string" ||
+      bookmark.category !== typeof "string" ||
+      bookmark.is_favorite !== typeof "boolean"
+    ) {
+      throw "You must specify a string or boolean";
+    }
+    const newBookmark = await db.one(
+      "INSERT INTO bookmarks (name, url, category, is_favorite) VALUES($1, $2, $3, $4) RETURNING *",
+      [bookmark.name, bookmark.url, bookmark.category, bookmark.is_favorite],
+    );
+    return newBookmark;
+  } catch (err) {
+    return err;
+  }
+};
+
 module.exports = {
   getAllBookmarks,
+  getBookmark,
+  createBookmark,
 };
